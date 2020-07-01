6387d576e0798aa1f953232621d5ddabb7e245aa
Derek Van Dyke <dvandyke@salesforce.com>
2020-06-30 18:46:35 -0700

    Update config to reference lighthouse heroku server

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,7 +5,9 @@ module.exports = {
       startServerCommand: "node app.js",
     },
     upload: {
-      target: "temporary-public-storage",
+      target: "lhci",
+      serverBaseUrl: "https://fierce-hamlet-72426.herokuapp.com",
+      token: "e19499c7-c41b-4fb7-8221-1b3641ce8478",
     },
   },
 };
