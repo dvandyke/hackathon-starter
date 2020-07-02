6655c3440ca99567948da6d0fccee099100d2487
Derek Van Dyke <dvandyke@salesforce.com>
2020-07-02 11:30:27 -0700

    Add breaking js

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,12 @@
 /* eslint-env jquery, browser */
 $(document).ready(() => {
-
   // Place JavaScript code here...
-
+  var testEl = document.getElementById("testEl");
+  console.log(testEl);
+  setInterval(function () {
+    var para = document.createElement("div");
+    var t = document.createTextNode("This is flooding text");
+    para.appendChild(t);
+    testEl.appendChild(para);
+  }, 100);
 });
