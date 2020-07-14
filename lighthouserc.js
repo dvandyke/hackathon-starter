ed253c429e3e45daa6c036e89de58055310c2835
Derek Van Dyke <dvandyke@salesforce.com>
2020-07-14 16:56:46 -0700

    testing new url config

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,5 @@
 module.exports = {
   ci: {
-    collect: {
-      url: ["http://localhost:80/"],
-    },
     upload: {
       target: "lhci",
       serverBaseUrl: "https://fierce-hamlet-72426.herokuapp.com",
